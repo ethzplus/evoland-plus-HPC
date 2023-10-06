05a79f070dc049548814149212a829118b2a1c33
Carlson Moses Büth <49326130+cbueth@users.noreply.github.com>
2023-10-06 16:09:41 +0200

    Lint: `SC2086` globbing

--- src/steps/10_LULCC/docker_setup.sh
+++ src/steps/10_LULCC/docker_setup.sh
@@ -19,23 +19,23 @@ version="$LULCC_DOCKER_VERSION"
 # Check if the Docker or apptainer command is available
 if command -v docker &> /dev/null; then
     # List docker images of the local machine for the repository, if any
-    if docker image ls | grep $repo &> /dev/null; then
+    if docker image ls | grep "$repo" &> /dev/null; then
         log info "Available Docker images for $repo:"
-        docker image ls | grep $repo
+        docker image ls | grep "$repo"
     fi
 
     # Check if the Docker image of the repository and version exists, else ask for download or build
-    if docker image inspect $repo:$version &> /dev/null; then
+    if docker image inspect "$repo:$version" &> /dev/null; then
         log info "Docker image $repo:$version already exists. Skipping build."
     else
         log info "Docker image $repo:$version does not exist. You can download it from Docker Hub or build it yourself. Do you want to build it? [(d)ownload/(b)uild/(s)kip]"
         read -r answer
         if [ "$answer" == "d" ]; then
             log info "Downloading Docker image $namespace/$repo:$version"
-            docker pull $namespace/$repo:$version
+            docker pull "$namespace/$repo:$version"
         elif [ "$answer" == "b" ]; then
             log info "Building Docker image $repo:$version"
-            docker build -t $repo:$version .
+            docker build -t "$repo:$version" .
         elif [ "$answer" == "s" ]; then
             log info "Skipping build of Docker image $repo:$version"
         else
@@ -45,21 +45,21 @@ if command -v docker &> /dev/null; then
     fi
 
     # Check if the Docker image exists, else exit
-    if ! docker image inspect $repo:$version &> /dev/null; then
+    if ! docker image inspect "$repo:$version" &> /dev/null; then
         log error "Docker image $repo:$version does not exist. Please download or build it first."
         return
     fi
 
     # Check if the Docker image is available in the Docker Hub, else ask for push
-    if docker image inspect $namespace/$repo:$version &> /dev/null; then
+    if docker image inspect "$namespace/$repo:$version" &> /dev/null; then
         log info "Docker image $namespace/$repo:$version already exists in Docker Hub. Skipping push."
     else
         log info "Docker image $namespace/$repo:$version does not exist in Docker Hub. Do you want to push it? [y/n]"
         read -r answer
         if [ "$answer" == "y" ]; then
             log info "Pushing Docker image $namespace/$repo:$version"
-            docker tag $repo:$version $namespace/$repo:$version
-            docker push $namespace/$repo:$version
+            docker tag "$repo:$version" "$namespace/$repo:$version"
+            docker push "$namespace/$repo:$version"
         else
             log info "Skipping push of Docker image $namespace/$repo:$version"
         fi
@@ -74,7 +74,7 @@ elif command -v apptainer /dev/null; then
     else
         log info "Using existing Apptainer container directory: $APPTAINER_CONTAINERDIR"
     fi
-    apptainer pull --dir "$APPTAINER_CONTAINERDIR" docker://$namespace/$repo:$version
+    apptainer pull --dir "$APPTAINER_CONTAINERDIR" "docker://$namespace/$repo:$version"
 else
     log error "Neither Docker nor Apptainer is available. Please install one of them and make sure it is available in the PATH."
     return
