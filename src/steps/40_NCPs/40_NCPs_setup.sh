f0adf1cdc873b89d7f6201f36f6fecacd2c652a3
Carlson Moses Büth <49326130+cbueth@users.noreply.github.com>
2024-04-15 17:08:19 +0200

    `20_FocalLULC`, `40_NCPs`: Reduce conda dependencies to one file

--- src/steps/40_NCPs/40_NCPs_setup.sh
+++ src/steps/40_NCPs/40_NCPs_setup.sh
@@ -9,12 +9,12 @@ source "$SCRIPT_DIR/../../bash_common.sh"
 
 # Create the conda environment
 log info "Creating conda environment: ncps"
-log debug "Using requirement file: $SCRIPT_DIR/40_NCPs_requirements.txt"
+log debug "Using requirement file: $SCRIPT_DIR/40_NCPs_env.yml"
 # If $CONDA_BIN is conda print warning that it's solver might have problems with the environment.yml file
 if [[ $CONDA_BIN == *conda ]]; then
-    log warning "Conda might have problems with the 40_NCPs_requirements.txt file. Use micromamba instead, or add the libmamba solver (https://conda.github.io/conda-libmamba-solver/getting-started/)"
+    log warning "If conda has problems with the 40_NCPs_env.yml file, use micromamba instead, or add the libmamba solver (https://conda.github.io/conda-libmamba-solver/getting-started/)"
 fi
-$CONDA_BIN create -n ncps -c conda-forge --file "$SCRIPT_DIR"/40_NCPs_requirements.txt -y
+$CONDA_BIN create -n ncps -c conda-forge --file "$SCRIPT_DIR"/40_NCPs_env.yml -y
 
 # Activate the conda environment
 log debug "Activating conda environment: ncps"
@@ -25,7 +25,3 @@ source "$SCRIPT_DIR/../../de_activate.sh" ncps 1
 # ecocrop       : https://github.com/cropmodels/Recocrop
 log info "Installing meteor and ecocrop R packages"
 R -e "pak::pkg_install(c('meteor', 'cropmodels/Recocrop'), upgrade = FALSE)"
-
-# Export the conda environment
-log debug "Exporting conda environment: ncps"
-$CONDA_BIN env export | grep -v "^prefix: " > "$SCRIPT_DIR"/40_NCPs_env.yml
